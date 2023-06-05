7c310786bb903b337fdab4c65b12cc107faa255a
Surprimo <alepasdie@alu.edu.gva.es>
2023-06-05 10:28:25 +0200

    Clase CalculMental con el método calcula

---
+++ src/es/iesmz/ed/algoritmes/CalculMental.java
@@ -0,0 +1,38 @@
+package es.iesmz.ed.algoritmes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CalculMental {
+    private String sequence;
+
+    public CalculMental(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public List<Integer> calcula() {
+        List<Integer> results = new ArrayList<>();
+
+        String[] operations = sequence.split("\\s+"); // Divide la secuencia en operaciones separadas
+
+        int partialResult = Integer.parseInt(operations[0]); // Inicializa el resultado parcial con el primer operando
+
+        for (int i = 1; i < operations.length; i += 2) {
+            String operator = operations[i]; // Obtiene el operador siguiente (+ o -)
+            int operand = Integer.parseInt(operations[i + 1]); // Obtiene el siguiente operando
+
+            if (operator.equals("+")) {
+                partialResult += operand;
+            } else if (operator.equals("-")) {
+                partialResult -= operand;
+            }
+
+            results.add(partialResult); // Agrega el resultado parcial a la lista de resultados
+        }
+
+        return results;
+    }
+}
+
+
