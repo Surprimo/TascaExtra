9aa175e1db8f3d1f02d75137d802a00d13566921
Surprimo <alepasdie@alu.edu.gva.es>
2023-06-09 18:45:27 +0200

    Primer test SudokuTest Correcto y variables sudoku para usar posteriormente en otros test más documentacion

--- src/es/iesmz/ed/algoritmes/SudokuTest.java
+++ src/es/iesmz/ed/algoritmes/SudokuTest.java
@@ -1,5 +1,79 @@
 package es.iesmz.ed.algoritmes;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class SudokuTest {
-    //Your code here
+    // Definición de sudokus para las pruebas
+
+    /**
+     * Sudoku correcto.
+     */
+    private int[][] sudokuCorrecto = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}
+    };
+
+    /**
+     * Sudoku con una fila incorrecta.
+     */
+    private int[][] sudokuFilaIncorrecta = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 2} // Última casilla incorrecta (2 repetido)
+    };
+
+    /**
+     * Sudoku con una columna incorrecta.
+     */
+    private int[][] sudokuColumnaIncorrecta = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9},
+            {5, 4, 6, 1, 7, 8, 3, 9, 2} // Última casilla incorrecta (2 repetido)
+    };
+
+    /**
+     * Sudoku con una región incorrecta.
+     */
+    private int[][] sudokuRegioIncorrecta = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9},
+            {1, 4, 2, 3, 6, 7, 5, 9, 8} // Última casilla incorrecta (1 repetido)
+    };
+
+    // Pruebas unitarias
+
+    /**
+     * Prueba unitaria para verificar un sudoku correcto.
+     */
+    @Test
+    public void testSudokuCorrecto() {
+        Sudoku sudoku = new Sudoku(sudokuCorrecto);
+        assertTrue(sudoku.esCorrecta());
+    }
 }
