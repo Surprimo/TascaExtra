f0b67c5971bde330b21310dde9bed23e39436ca3
Surprimo <alepasdie@alu.edu.gva.es>
2023-06-09 17:57:55 +0200

    Sudoku método esValida

--- src/es/iesmz/ed/algoritmes/Sudoku.java
+++ src/es/iesmz/ed/algoritmes/Sudoku.java
@@ -1,7 +1,30 @@
 package es.iesmz.ed.algoritmes;
 
 
-public class Sudoku{
-    //Your code here
+public class Sudoku {
+    private int[][] cledas;
+
+    public Sudoku(int[][] cledas) {
+        this.cledas = cledas;
+    }
+
+
+    /**
+     * Método para acomprobar si el array introducido es correcto
+     * Se crea un array booleano para comprobar cada número del array introducido
+     * @param array se usa el array introducido en un foreach para comprobar cada iteracion si es correcto el número
+     * @return devuelve false si los números son menores de 1 o mayores de 9 y si un número se repite en el array, en caso de ser correcto devuelve true
+     */
+    private boolean esValida(int[] array) {
+        boolean[] nums = new boolean[9];
+        for (int num : array) {
+            if (num < 1 || num > 9 || nums[num - 1]) {
+                return false;
+            }
+            nums[num - 1] = true;
+        }
+        return true;
+    }
 }
+
 
